f785e4b0b8cf79992abed9756d3010214cd93021
Nya Candy <dev@candinya.com>
2023-01-25 11:06:14 +0800

    chore: add comment about consent csrf

--- consts/redis_key.go
+++ consts/redis_key.go
@@ -1,7 +1,7 @@
 package consts
 
 const (
-	REDIS_KEY_LOGIN_SESSION = "misso:login:%s" // Username, token as value
-	REDIS_KEY_CONSENT_CSRF  = "misso:consent:%s"
-	REDIS_KEY_SHARE_CONTEXT = "misso:share:%s" // Subject, context as value
+	REDIS_KEY_LOGIN_SESSION = "misso:login:%s"   // Username, token as value
+	REDIS_KEY_CONSENT_CSRF  = "misso:consent:%s" // Random string, consent challenge as value
+	REDIS_KEY_SHARE_CONTEXT = "misso:share:%s"   // Subject, context as value
 )
